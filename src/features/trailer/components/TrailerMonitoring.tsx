1e875d839b64b15db552e50992b72c89e5f0f223
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-10-10 20:08:45 +0530

    latest trailer

--- src/features/trailer/components/TrailerMonitoring.tsx
+++ src/features/trailer/components/TrailerMonitoring.tsx
@@ -7,19 +7,30 @@ import useTrailerMonitoring from "../hooks/useTrailerMonitoring"
 
 export default function TrailerMonitoring() {
 	const { messages } = useTrailerMonitoring()
+	const firstObj = messages[0]
 
+	if (!firstObj) return (
+		<div>No Data</div>
+	)
+	console.log(firstObj)
 	return (
-		<Dialog>
-			<DialogTrigger className="cursor-pointer hover:text-blue-500">
-				<BarChartIcon size={20} />
-			</DialogTrigger>
-			<DialogContent className="min-w-[90vw]  max-h-[90vh] overflow-y-auto rounded-4xl bg-black/20 dark:bg-black/10 backdrop-blur-xl border border-white/20 dark:border-white/10 shadow-2xl shadow-black/10 dark:shadow-black/30">
-				<DialogTitle className="flex gap-3 items-center text-xl font-semibold text-gray-900 dark:text-gray-100 mb-6 text-white">
-					<Power className="h-5 w-5 text-white" />
-					Switch State Monitoring
-				</DialogTitle>
-			</DialogContent>
-		</Dialog>
+		<div>
+			<div className="p-10">
+				{firstObj.battery.current_level}
+			</div>
+			<Dialog>
+				<DialogTrigger className="cursor-pointer hover:text-blue-500">
+					<BarChartIcon size={20} />
+				</DialogTrigger>
+				<DialogContent className="min-w-[90vw]  max-h-[90vh] overflow-y-auto rounded-4xl bg-black/20 dark:bg-black/10 backdrop-blur-xl border border-white/20 dark:border-white/10 shadow-2xl shadow-black/10 dark:shadow-black/30">
+					<DialogTitle className="flex gap-3 items-center text-xl font-semibold text-gray-900 dark:text-gray-100 mb-6 text-white">
+						<Power className="h-5 w-5 text-white" />
+						Switch State Monitoring
+					</DialogTitle>
+				</DialogContent>
+			</Dialog>
+
+		</div>
 	)
 }
 
